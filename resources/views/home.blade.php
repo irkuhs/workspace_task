a57e61b6fb2e6fdc2bc244d22c082b532c723186
irkuhs <shukriahmad56@gmail.com>
2023-03-22 15:01:05 +0800

    Others: Susun balik hardcode

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,7 @@
                                     <tr>
                                         <th scope="col">#</th>
                                         <th scope="col">Name</th>
-                                        <th scope="col">Date</th>
-                                        <th scope="col">Time</th>
+                                        <th scope="col">Date Time</th>
                                         <th scope="col">Status</th>
                                         <th scope="col">Action</th>
                                     </tr>
@@ -28,7 +27,6 @@
                                         <td>Otto</td>
                                         <td>@mdo</td>
                                         <td>@mdo</td>
-                                        <td>@mdo</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">2</th>
@@ -36,12 +34,10 @@
                                         <td>Thornton</td>
                                         <td>@fat</td>
                                         <td>@fat</td>
-                                        <td>@fat</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">3</th>
                                         <td colspan="2">Larry the Bird</td>
-                                        <td>@twitter</td>
                                         <td>@twitter</td>
                                         <td>@twitter</td>
                                     </tr>
@@ -55,15 +51,29 @@
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="addWorkspaceModal">Modal title</h1>
+                                <h1 class="modal-title fs-5" id="addWorkspaceModal">Workspace Task Baru</h1>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                ...
+                                    <form>
+                                        <div class="mb-3">
+                                            <label class="col-form-label">Name</label>
+                                            <input type="text" class="form-control" id="name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="col-form-label">Date</label>
+                                            <input type="datetime-local" class="form-control" name="datetime">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="col-form-label">Status:</label>
+                                            <input type="text" class="form-control" id="status"></textarea>
+                                          </div>
+                                      </form>
                                 </div>
                                 <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-outline-primary">Add</button>
+                                    <button type="button" class="btn btn-outline-danger">Clear</button>
                                 </div>
                             </div>
                         </div>
