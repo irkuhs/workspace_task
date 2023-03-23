a82fc5581e9a2e8ca460ada65146e57cb3ccdc1b
irkuhs <shukriahmad56@gmail.com>
2023-03-23 14:53:51 +0800

    Add: relation

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -24,5 +25,8 @@ class Workspace extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-
+    public function task()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
