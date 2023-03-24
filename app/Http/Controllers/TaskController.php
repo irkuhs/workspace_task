94adfcb2bf058b932e1791384b5e77826dd7d201
irkuhs <shukriahmad56@gmail.com>
2023-03-24 11:54:30 +0800

    Edit: due date compare (trying)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,31 +5,29 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\Workspace;
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskRequest;
 
 class TaskController extends Controller
 {
-    public function show()
+    public function create(Workspace $workspace)
     {
-        return view('task.show');
-    }
-
-    public function create()
-    {
-        return view('task.create');
+        return view('task.create', compact('workspace'));
     }
 
     public function store(Request $request, Workspace $workspace)
     {
+
+        $duedate['due_date'] = $workspace->datetime;
+
         Task::create(
             [
-                'workspace_id'=> $workspace->id,
-                'user_id' => auth()->user()->id,
+                'workspaces_id'=> $workspace->id,
                 'name'=> $request-> name,
                 'datetime' => $request -> datetime,
-                'status' => $request->status,
+                // 'status' => $request->status,
             ]
             );
 
-        return to_route("workspace.show");
+        return to_route("workspace.show", compact('workspace'));
     }
 }
