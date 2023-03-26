6bfe76f209c8ebbd00b2a67bb89b54409226dc5e
irkuhs <shukriahmad56@gmail.com>
2023-03-26 22:28:02 +0800

    Add: error message

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -5,9 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Add New Type of Task</div>
+                <div class="card-header">Add New Task dari {{$workspace->name}}</div>
 
                 <div class="card-body">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <form method="POST" action="{{ route('task.store', $workspace) }}">
                         @csrf
                         <div class="mb-3">
@@ -15,21 +26,17 @@
                             <input type="text" class="form-control" name="name" placeholder="Workspace Name">
                         </div>
                         <div class="mb-3">
-                            <label class="col-form-label">Date and Time</label>
+                            <label class="col-form-label">Before Workspace Due Date :  {{$workspace->datetime}}</label>
                             <input type="datetime-local" class="form-control" name="datetime">
                         </div>
-                        <div class="mb-3">
+                        {{-- <div class="mb-3">
                             <label class="col-form-label">Status:</label>
-                            <select name="status" class="form-control" id="status">
-                                <option selected value="" disabled selected hidden></option>
-                                <option value="0">Done</option>
-                                <option value="1">Pending</option>
-                            </select>
-                        </div>
+                            <input type="text" class="form-control" name="status" value="pending" placeholder="pending" readonly>
+                        </div> --}}
                         <div class="modal-footer">
-                            <a href="{{ route('workspace.show') }}" button type="button" class="btn btn-outline-secondary">Back</button></a>
+                            <a href="{{ route('workspace.show', $workspace) }}" button type="button" class="btn btn-outline-secondary">Back</button></a>
                             <button type="submit" class="btn btn-outline-primary">Add</button>
-                            <button type="button" class="btn btn-outline-danger">Clear</button>
+                            <button type="reset" class="btn btn-outline-danger">Clear</button>
                         </div>
                     </form>
                 </div>
