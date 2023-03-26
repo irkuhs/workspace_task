ee5aa9eb4aebae86828c2edd1066bb32f7699f54
irkuhs <shukriahmad56@gmail.com>
2023-03-26 22:28:26 +0800

    Add: Button edit

--- resources/views/workspace/show.blade.php
+++ resources/views/workspace/show.blade.php
@@ -8,7 +8,8 @@
                 <div class="card-header">Task dari {{$workspace->name}}</div>
                     <div class="card-body">
                         <a href="{{route('home')}}" button type="button" class="btn btn-outline-danger">Back</button></a>
-                        <a href="{{route('task.', $workspace)}}" button type="button" class="btn btn-outline-primary">Add Task</button></a>
+                        <a href="{{route('task.create', $workspace)}}" button type="button" class="btn btn-outline-primary">Add Task</button></a>
+                        <a href="" button type="button" class="btn btn-outline-info">Edit</button></a>
                     </div>
                     <table class="table table-hover">
                         <thead>
@@ -21,12 +22,18 @@
                             </tr>
                         </thead>
                         <tbody>
-                                <tr>
-                                    <td>
-                                        <a href="" button type="button" class="btn btn-outline-info">Edit</button></a>
-                                        <a href="" button type="button" class="btn btn-outline-warning">Delete</button></a>
-                                    </td>
-                                </tr>
+                            @foreach ( $task as $key => $task)
+                                    <tr>
+                                        <th scope="row">{{ $key+1}}</th>
+                                        <td>{{ $task->name}}</td>
+                                        <td>{{ $task->datetime}}</td>
+                                        <td>{{ $task->status}}</td>
+                                        <td>
+                                            <a href="" button type="button" class="btn btn-outline-danger">Show</button></a>
+                                            <a href="{{ route('task.delete', [$workspace,$task]) }}" button type="button" class="btn btn-outline-warning">Delete</button></a>
+                                        </td>
+                                    </tr>
+                            @endforeach
                         </tbody>
                     </table>
             </div>
