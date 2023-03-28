bd644466be44c79edfa49e94bc8d0e1aece7c623
irkuhs <shukriahmad56@gmail.com>
2023-03-28 15:33:53 +0800

    Create: Task policy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use App\Models\Workspace;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(Workspace $workspace, Task $task)
+    {
+        return $workspace->id === $task->workspace_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(Workspace $workspace)
+    {
+        return $workspace->id === $task->workspace_id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(Workspace $workspace, Task $task)
+    {
+        return $workspace->id === $task->workspace_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(Workspace $workspace, Task $task)
+    {
+        return $workspace->id === $task->workspace_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(Workspace $workspace, Task $task)
+    {
+        return $workspace->id === $task->workspace_id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(Workspace $workspace, Task $task)
+    {
+        return $workspace->id === $task->workspace_id;
+    }
+}
