2a3db35431c65c022182ccdf9fc7272641380a58
irkuhs <shukriahmad56@gmail.com>
2023-03-28 11:34:45 +0800

    Add: Error message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,6 +58,15 @@
                                 <div class="modal-body">
                                     <form method="POST" action="{{ route('workspace.store') }}">
                                         @csrf
+                                        @if ($errors->any())
+                                            <div class="alert alert-danger">
+                                                <ul>
+                                                    @foreach ($errors->all() as $error)
+                                                        <li>{{ $error }}</li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                        @endif
                                         <div class="mb-3">
                                             <label class="col-form-label">Name</label>
                                             <input type="text" class="form-control" name="name" placeholder="Workspace Name">
