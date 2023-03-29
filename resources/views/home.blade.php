b7aa2402c1619feaa6467803980cc4cff1eed7ab
irkuhs <shukriahmad56@gmail.com>
2023-03-29 15:21:39 +0800

    Other: Organize button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,16 @@
                 <div class="card-header">{{ __('Dashboard milik ')}}{{ Auth::user()->name }}</div>
                     <div class="card-body">
                         <form action="{{route('workspace.search')}}" method="GET">
-                            <div class="input-group-append mt-2 p-2">
+                            <div class="input-group-append mt-2 p-1">
                                 <input type="text" class="form-control" name="keyword" placeholder="Search by Inventory Name">
                                 <div class="input-group-append mt-2">
                                     <button class="btn btn-primary" type="submit">Search</button>
                                     <a href="{{route('home')}}" button class="btn btn-primary" type="submit">Refresh</button></a>
-                                    <!-- Button trigger modal -->
-                                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#addWorkspaceModal">Add Work</button>
                                 </div>
                             </div>
                         </form>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#addWorkspaceModal">Add Work</button>
                         <div class="card mt-2">
                             <table class="table table-hover">
                                 <thead>
