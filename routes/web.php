7a3240b6b065fe50c2927be345f04d188fe71f57
irkuhs <shukriahmad56@gmail.com>
2023-03-30 10:47:53 +0800

    Add: uuid on route

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::post('/workspace/store', [App\Http\Controllers\WorkspaceController::class, 'store'])->name('workspace.store');
 Route::get('/workspace/show/{workspace:uuid}', [App\Http\Controllers\WorkspaceController::class, 'show'])->name('workspace.show');
 Route::get('/workspace/delete/{workspace}', [App\Http\Controllers\WorkspaceController::class, 'delete'])->name('workspace.delete');
-Route::get('/workspace/edit/{workspace}', [App\Http\Controllers\WorkspaceController::class, 'edit'])->name('workspace.edit');
+Route::get('/workspace/edit/{workspace:uuid}', [App\Http\Controllers\WorkspaceController::class, 'edit'])->name('workspace.edit');
 Route::post('/workspace/update/{workspace}', [App\Http\Controllers\WorkspaceController::class, 'update'])->name('workspace.update');
 Route::get('/workspace/search', [App\Http\Controllers\WorkspaceController::class, 'search'])->name('workspace.search');
 
 //task
-Route::get('/task/create/{workspace}', [App\Http\Controllers\TaskController::class, 'create'])->name('task.create');
+Route::get('/task/create/{workspace:uuid}', [App\Http\Controllers\TaskController::class, 'create'])->name('task.create');
 Route::post('/task/store/{workspace}', [App\Http\Controllers\TaskController::class, 'store'])->name('task.store');
 Route::get('/task/delete/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'delete'])->name('task.delete');
 Route::get('/task/status/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'status'])->name('task.status');
-Route::get('/task/edit/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'edit'])->name('task.edit');
+Route::get('/task/edit/{workspace:uuid}/{task:uuid}', [App\Http\Controllers\TaskController::class, 'edit'])->name('task.edit');
 Route::post('/task/update/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'update'])->name('task.update');
